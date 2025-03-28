e581df6ffd4a5b1be8e1e956ad5a1b1564500730
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2025-03-28 17:24:00 -0500

    Prisma

--- anylist/src/app.module.ts
+++ anylist/src/app.module.ts
@@ -1,12 +1,16 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ItemsModule } from './items/items.module';
-import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
